28c0d050022c223e4ddeb8a4222f366559fbec9b
syaiful9507 <syaiful9507@gmail.com>
2017-07-14 09:46:22 +0700

    jul 14 09:45

--- app/src/main/java/syaiful/code/collapsetollbar/MainActivity.java
+++ app/src/main/java/syaiful/code/collapsetollbar/MainActivity.java
@@ -1,9 +1,14 @@
 package syaiful.code.collapsetollbar;
 
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CollapsingToolbarLayout collapsingToolbarLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
